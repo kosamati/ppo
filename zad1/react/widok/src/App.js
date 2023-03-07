1b6d3b711364587f0959b70be5ffec8bf9964622
kosamati <72787801+kosamati@users.noreply.github.com>
2023-03-07 11:04:19 +0100

    5

--- zad1/react/widok/src/App.js
+++ zad1/react/widok/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Account from './components/Account';
 import Person from './components/Person';
+import PersonPost from "./components/PersonPost"
 
 function App() {
   return (
     <div className="App">
-      <Person personID={1} />
+      <PersonPost  />
     </div>
   );
 }
