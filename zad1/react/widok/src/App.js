81d7c975606a2065c6cb38b2efc54d007678f02b
kosamati <72787801+kosamati@users.noreply.github.com>
2023-03-14 13:54:46 +0100

    2.2

--- zad1/react/widok/src/App.js
+++ zad1/react/widok/src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-// read
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import Account from './components/get/Account';
 import Person from './components/get/Person';
 import ClientView from './components/get/Client';
-import Bank from './components/get/Bank'
-import PersonList from './components/get/PersonList'
+import Bank from './components/get/Bank';
+import PersonList from './components/get/PersonList';
 
 import PersonPost from "./components/post/PersonPost";
 import BankPost from "./components/post/Bankpost";
 import ClientPost from './components/post/ClientPost';
 import AccountPost from './components/post/AccountPost';
-
 
 import DeletePerson from './components/delete/PersonDelete';
 import DeleteAccount from './components/delete/DeleteAccount';
@@ -22,12 +22,35 @@ import UpdateBank from './components/Update/UpdateBank';
 import UpdateClient from './components/Update/UpdateClient';
 import UpdatePerson from './components/Update/UpdatePerson';
 
-
 function App() {
   return (
-    <div className="App">
-      <Account accountId="2"/>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/personlist" component={PersonList} />
+
+          <Route path="/account/:accountId" component={Account} />
+          <Route path="/person/:personId" component={Person} />
+          <Route path="/client/:clientId" component={ClientView} />
+          <Route path="/bank/:bankId" component={Bank} />
+          
+          <Route path="/create-person" component={PersonPost} />
+          <Route path="/create-bank" component={BankPost} />
+          <Route path="/create-client" component={ClientPost} />
+          <Route path="/create-account" component={AccountPost} />
+
+          <Route path="/delete-person/" component={DeletePerson} />
+          <Route path="/delete-account/" component={DeleteAccount} />
+          <Route path="/delete-bank/" component={BankDelete} />
+          <Route path="/delete-client/" component={DeleteClient} />
+
+          <Route path="/update-account/:accountId" component={UpdateAccount} />
+          <Route path="/update-bank/:bankId" component={UpdateBank} />
+          <Route path="/update-client/" component={UpdateClient} />
+          <Route path="/update-person/" component={UpdatePerson} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
